27e1c75927231dcad637e505e387d0a15e012d62
shiba6v <shiba6v@gmail.com>
2022-12-17 01:40:55 +0900

    up

--- detect.sh
+++ detect.sh
@@ -1,15 +1,20 @@
 set -u
-COMPARE_FILE=$1
-BASE_BRANCH=$2
+ORIGINAL_BRANCH=$1
+UPDATE_FILE=$2
 
 # pip install git+https://github.com/googleapis/proto-breaking-change-detector.git
 
-TMP_FILE=$(uuidgen)
-git show ${BASE_BRANCH}:${COMPARE_FILE} > tmp/${TMP_FILE}.proto
+# 適当なディレクトリにcloneする
+TMP_DIR=/tmp/$(uuidgen)
+mkdir -p ${TMP_DIR}
+git clone --depth=1 -b ${ORIGINAL_BRANCH} $(git remote get-url $(git remote)) ${TMP_DIR}
+ORIGINAL_FILE=${TMP_DIR}/${UPDATE_FILE}
 
 PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python proto-breaking-change-detector \
---original_api_definition_dirs  \
---update_api_definition_dirs ${COMPARE_FILE} \
+--original_api_definition_dirs=$(dirname ${ORIGINAL_FILE}) \
+--update_api_definition_dirs=$(dirname ${UPDATE_FILE}) \
+--original_proto_files=${ORIGINAL_FILE} \
+--update_proto_files=${UPDATE_FILE} \
 --human_readable_message
 
-rm tmp/${TMP_FILE}.proto
+rm -rf ${TMP_DIR}
