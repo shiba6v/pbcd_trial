dfebc5faa27ef9f9cef635e66e3ccfe415cdc94a
shiba6v <shiba6v@gmail.com>
2022-12-17 02:01:18 +0900

    up

--- detect.sh
+++ detect.sh
@@ -11,7 +11,13 @@ UPDATE_FILE=proto/hoge/hoge.proto,proto/fuga/fuga.proto
 TMP_DIR=/tmp/$(uuidgen)
 mkdir -p ${TMP_DIR}
 git clone --depth=1 -b ${ORIGINAL_BRANCH} $(git remote get-url $(git remote)) ${TMP_DIR}
-ORIGINAL_FILE=${TMP_DIR}/${UPDATE_FILE}
+
+# proto_pathのディレクトリを${TMP_DIR}に書き換える
+ORIGINAL_PROTO_PATH=$(echo ${UPDATE_PROTO_PATH} | tr "," "\n" | xargs -I{} echo "${TMP_DIR}/{}" | tr "\n" "," | sed 's/,$//')
+ORIGINAL_FILE=$(echo ${UPDATE_FILE} | tr "," "\n" | xargs -I{} echo "${TMP_DIR}/{}"  | tr "\n" "," | sed 's/,$//')
+
+# echo ${ORIGINAL_PROTO_PATH}
+# echo ${ORIGINAL_FILE}
 
 # --original_api_definition_dirs: 比較元の--proto-pathに指定するディレクトリをカンマ区切りで
 # --original_proto_files: 比較元のbeaking changeを検出したいファイルをカンマ区切りで
@@ -19,8 +25,8 @@ ORIGINAL_FILE=${TMP_DIR}/${UPDATE_FILE}
 # --update_proto_files: 比較先
 
 PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python proto-breaking-change-detector \
-    --original_api_definition_dirs=$(dirname ${ORIGINAL_FILE}) \
-    --update_api_definition_dirs=$(UPDATE_PROTO_PATH) \
+    --original_api_definition_dirs=${ORIGINAL_PROTO_PATH} \
+    --update_api_definition_dirs=${UPDATE_PROTO_PATH} \
     --original_proto_files=${ORIGINAL_FILE} \
     --update_proto_files=${UPDATE_FILE} \
     --human_readable_message
